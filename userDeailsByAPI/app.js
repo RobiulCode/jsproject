65b4cc10ac2405a01ab40fb47272fc90724da399
RobiulCode <robiulawal68@gmail.com>
2022-01-29 19:28:31 +0600

    UserDetailsApp Complete

--- userDeailsByAPI/app.js
+++ userDeailsByAPI/app.js
@@ -1,19 +1,65 @@
-const search = document.getElementById("searchBtn");
-search.addEventListener("click",()=>{
-    fetch("https://jsonplaceholder.typicode.com/users")
+//Call load All Data
+fetch("https://jsonplaceholder.typicode.com/users")
     .then(res => res.json())
     .then(data =>{
-        data.forEach(user => {
-            const username = user.name;
-            const userId =user.id;
-            const userList = document.getElementById("user-list");
-            const listItem = document.createElement("li");
-            listItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-content-center");
-            listItem.innerHTML = `
-                                <span class="">${username}</span>
-                                <span class="badge bg-primary rounded">Details</span>
-                            `
-            userList.appendChild(listItem);
-        });
+        loadData(data)
     })
+//-----------------------------------------------------------
+
+//specific count data call button
+const search = document.getElementById("searchBtn");
+
+search.addEventListener("click",()=>{
+    const input = document.getElementById("input").value;
+    const inputCount = parseInt(input);
+    if (inputCount>0) {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => res.json())
+        .then(data =>{
+        data = data.slice(0,inputCount)
+        const userList = document.getElementById("user-list");
+        userList.innerHTML = "";
+        loadData(data)
+    })
+    } else {
+        alert("Please inset a number")
+    }
 })
+//------------------------------------------------------------
+
+//Data load function
+function loadData(data){
+    data.forEach(user => {
+        const username = user.name;
+        const userId =user.id;
+        const userList = document.getElementById("user-list");
+        const listItem = document.createElement("li");
+        listItem.classList.add("list-group-item", "d-flex", "justify-content-between", "align-content-center");
+        listItem.innerHTML = `
+                            <span class="">${username}</span>
+                            <button class="btn badge bg-primary rounded" onclick="userDetails(${userId})">Details</button>
+                        `
+        userList.appendChild(listItem);
+    });
+}
+//Single User Data Load Function
+function userDetails(userId){
+    fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
+    .then(res => res.json())
+    .then(user =>{
+        const name = user.name;
+        const email = user.email;
+        const phone = user.phone;
+        const website =user.website;
+        const address = `${user.address.street},${user.address.suite},${user.address.city},${user.address.zipcode}`
+        const userData = document.getElementById("user-data");
+        userData.innerHTML = `
+                            <li class="list-group-item"><strong>Name :</strong> ${name}</li>
+                            <li class="list-group-item"><strong>Email :</strong> ${email}</li>
+                            <li class="list-group-item"><strong>Phone :</strong> ${phone}</li>
+                            <li class="list-group-item"><strong>Website :</strong> ${website}</li>
+                            <li class="list-group-item"><strong>Address :</strong> ${address}</li>
+                            `
+
+    })
+}
