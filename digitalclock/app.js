f792c4093cc5a8bd2e68b4fa165c365ef30499f2
RobiulCode <robiulawal68@gmail.com>
2022-01-29 10:22:13 +0600

    New project from mobile

--- digitalclock/app.js
+++ digitalclock/app.js
@@ -1,28 +1,24 @@
 function digitalClock(){
-    //get unit
-    const date =new Date();
-    let hr = date.getHours();
-    let min = date.getMinutes();
-    let sec = date.getSeconds();
-    let year = date.getFullYear();
-    let status = "AM";
-    if(hr>12){
-        hr = hr-12;
-        status = "PM"
+    const date = new Date();
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    let seconds = date.getSeconds();
+    let format = "AM";
+    if(hours==0){
+        hours = 12;
     }
-    hr = (hr<10) ? "0"+hr : hr;
-    min = (min<10) ? "0"+min : min;
-    sec = (sec<10) ? "0"+sec : sec;
-    //day & month
-    let dayName = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-    let monthName = ["January","February","March","April","May","June","July","Auguest","September","Octobar","November","December"]
-    //dayInfo
-    let day = `${dayName[date.getDay()]}, ${date.getDate()} ${monthName[date.getMonth()]} ${year}`;
-    //UI display
-    document.getElementById("day").innerHTML = day;
-    document.getElementById("hr").innerHTML = hr;
-    document.getElementById("min").innerHTML = min;
-    document.getElementById("sec").innerHTML = sec;
-setInterval(digitalClock,1000)
+    if(hours > 12){
+        hours= hours-12;
+        format = "PM"
+    }
+    hours = hours < 10 ? '0'+ hours : hours;
+    minutes = minutes < 10 ? '0'+ minutes : minutes;
+    seconds = seconds < 10 ? '0'+ seconds : seconds;
+    let time = `${hours}:${minutes}:${seconds}`;
+    let clock = document.getElementById("clock");
+    let formatStatus = document.getElementById("format");
+    clock.innerHTML = time;
+    formatStatus.innerHTML = format;
+    setInterval(digitalClock,1000);
 }
 digitalClock();
---
+++ weatherApp/app.js
@@ -0,0 +1,12 @@
+const searchValue = document.getElementById("search-value").value;
+const search = document.getElementById("search");
+const countryName = document.getElementById("country-name");
+const temperature = document.getElementById("temperature");
+const description = document.getElementById("description");
+search.addEventListener("click",()=>{
+    fetch("")
+    .then(res => response.json())
+    .then(data =>{
+
+    })
+})
